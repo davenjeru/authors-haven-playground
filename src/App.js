df01e33118d186a89a788622c14b1e4a5bea3238
Dave Mathews Njeru <dave.njeru@andela.com>
2018-09-03 13:05:09 +0300

    Maintainability config (#2)
    * add pull request template
    
    * add eslint
    
    * add .eslintignore
    
    * add .hound.yml
    
    * add .travis.yml
    
    * add .codeclimate.yml
    
    * fix eslint issues in App.js
    
    * add .lint, .test and .db files to .gitignore
    
    * add 'babel-eslint' as the parser in eslintrc.json
    
    * remove 'npm run build' in .travis.yml
    
    * add state to the App component
    
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +16,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
       </div>
     );
