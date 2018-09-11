3094fe1bf6f2c92f64b950174b86dc6dc71e2b84
davenjeru <dave.njeru@andela.com>
2018-09-11 21:09:13 +0300

    [feature #160425678] add routing with dummy landing and not-found pages
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import LandingPage from './components/LandingPage';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route path="/" exact component={LandingPage} />
+    <Route path="*" component={NotFound} />
+  </Switch>
+);
 
 export default App;
