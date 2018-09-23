24c6a9d9d21f11e3585455829711a88b0c2c7054
davenjeru <dave.njeru@andela.com>
2018-09-23 19:42:40 +0300

    [Feature #160425661] add snapshot tests for the footer
    Signed-off-by: davenjeru <dave.njeru@andela.com>

---
+++ src/__tests__/commonsTests/footerSnapshotTests.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import Footer from '../../components/commons/Footer';
+
+describe('The Footer', () => {
+  it('should not regress', () => {
+    const tree = renderer.create(<Footer />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
